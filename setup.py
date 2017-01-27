dc740a09830073982f28adb2827b8bb7cab84568
Andrius Merkys <andrius.merkys@gmail.com>
2017-01-27 07:26:29 +0000

    trunk/ Building 'cexceptions' and 'codcif' using Makefile, as seen in https://github.com/mwilliamson/jq.py/blob/master/setup.py.
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/andrius/svn-repositories/pycodcif-Python-packing/trunk@7 24fe5eee-136a-442f-b492-3ec96a2c73c1

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 from setuptools import setup, find_packages, Extension
+from distutils.command.build_ext import build_ext
+import subprocess
 
 version = '1.0.0';
+
+class pycodcif_build_ext(build_ext):
+    def run(self):
+        subprocess.check_call(['make'],
+                              cwd='cod-tools/src/externals/cexceptions')
+        subprocess.check_call(['make'],
+                              cwd='cod-tools/src/components/codcif')
+        build_ext.run(self)
 
 setup(
     name="pycodcif",
@@ -14,22 +24,42 @@ setup(
     packages=find_packages(),
     url="http://wiki.crystallography.net/cod-tools",
     license="GPLv2",
+    cmdclass={'build_ext': pycodcif_build_ext},
     ext_modules=[
-        Extension('cexceptions',
-                  ['cod-tools/src/externals/cexceptions/allocx.c',
-                   'cod-tools/src/externals/cexceptions/cexceptions.c',
-                   'cod-tools/src/externals/cexceptions/cxprintf.c',
-                   'cod-tools/src/externals/cexceptions/stdiox.c',
-                   'cod-tools/src/externals/cexceptions/stringx.c'],
-                   include_dirs=['cod-tools/src/externals/cexceptions']),
+        # Extension('cexceptions',
+                  # ['cod-tools/src/externals/cexceptions/allocx.c',
+                   # 'cod-tools/src/externals/cexceptions/cexceptions.c',
+                   # 'cod-tools/src/externals/cexceptions/cxprintf.c',
+                   # 'cod-tools/src/externals/cexceptions/stdiox.c',
+                   # 'cod-tools/src/externals/cexceptions/stringx.c'],
+                   # include_dirs=['cod-tools/src/externals/cexceptions']),
+                   # 
+        # Extension('codcif', [
+                  # 'cod-tools/src/externals/cexceptions/allocx.c',
+                  # 'cod-tools/src/externals/cexceptions/cexceptions.c',
+                  # 'cod-tools/src/externals/cexceptions/cxprintf.c',
+                  # 'cod-tools/src/externals/cexceptions/stdiox.c',
+                  # 'cod-tools/src/externals/cexceptions/stringx.c',
+                  # 'cod-tools/src/components/codcif/cif.c',
+                  # 'cod-tools/src/components/codcif/cifmessage.c',
+                  # 'cod-tools/src/components/codcif/common.c',
+                  # 'cod-tools/src/components/codcif/yy.c',
+                  # 'cod-tools/src/components/codcif/cif_grammar_flex.c',
+                  # 'cod-tools/src/components/codcif/cif_lexer.c',
+                  # 'cod-tools/src/components/codcif/cif_options.c',
+                  # 'cod-tools/src/components/codcif/datablock.c',
+                  # 'cod-tools/src/externals/getoptions/getoptions.c'],
+                  # include_dirs=['cod-tools/src/externals/cexceptions',
+                                # 'cod-tools/src/externals/getoptions',
+                                # 'cod-tools/src/components/codcif'],
+                  # extra_compile_args=['-DYYDEBUG=1', '-D_YACC_']),
     
         Extension('_ccifparser',
                   ['cod-tools/src/lib/python2.7/dist-packages/cod/ccifparser/ccifparser.c',
                    'cod-tools/src/lib/python2.7/dist-packages/cod/ccifparser/ccifparser.i'],
                   include_dirs=['cod-tools/src/externals/cexceptions',
                                 'cod-tools/src/externals/getoptions',
-                                'cod-tools/src/components/codcif'],
-                  libraries=['codcif', 'cexceptions']),
+                                'cod-tools/src/components/codcif']),
 
         # Extension('codcif', [
                 # 'cod-tools/src/externals/cexceptions/allocx.c',
