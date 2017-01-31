67433e9d88ddce6dfef9ee95bbc263f1d75d4e52
Andrius Merkys <andrius.merkys@gmail.com>
2017-01-31 15:50:22 +0000

    branches/cod-tools-python-refactoring/ Changing version of the package to 0.1.0.
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/andrius/svn-repositories/pycodcif-Python-packing/branches/cod-tools-python-refactoring@22 24fe5eee-136a-442f-b492-3ec96a2c73c1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.command.build import build
 from distutils.command.build_ext import build_ext
 import subprocess
 
-version = '1.0.0';
+version = '0.1.0';
 
 class CustomBuild(build):
     def run(self):
